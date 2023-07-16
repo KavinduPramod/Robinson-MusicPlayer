d3e19d64b875cf35691e47d4265d9a1c2067e3ab
Kavindu Pramod <95103102+KavinduPramod@users.noreply.github.com>
2023-07-16 09:24:45 +0530

    Completed implementation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.robinson_music_player {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens com.example.robinson_music_player to javafx.fxml;
